91895626aa354abdeb89df290bc2a12d04746557
Tony <coolhzb@163.com>
2013-09-15 23:44:45 +0800

    add an util method

--- WeCampus/WeCampus/src/main/java/com/westudio/wecampus/util/HttpUtil.java
+++ WeCampus/WeCampus/src/main/java/com/westudio/wecampus/util/HttpUtil.java
@@ -1,10 +1,53 @@
 package com.westudio.wecampus.util;
 
+import android.os.Bundle;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by nankonami on 13-9-14.
  * A common http util to generate the request url and so on.
  */
 public class HttpUtil {
+
+    /**
+     * Build the url query string according to the param bundle
+     * @param params
+     * @return
+     */
+    private static String encodeUrl(Bundle params)
+    {
+        if(params == null)
+            return "";
+
+        StringBuilder sb = new StringBuilder();
+        boolean bFirst = true;
+
+        Set<String> setKeys = new HashSet<String>(params.keySet());
+
+        for(String strKey : setKeys)
+        {
+            if(bFirst)
+                bFirst = false;
+            else
+                sb.append("&");
+
+            try
+            {
+                sb.append(URLEncoder.encode(strKey, "UTF-8"))
+                        .append("=")
+                        .append(URLEncoder.encode(params.getString(strKey), "UTF-8"));
+            } catch (UnsupportedEncodingException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
+    }
 
     /**
      * Get the url for request activity
