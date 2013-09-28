f7f30af407ec980df9521df79e099ffaa929efaf
martin <nl.martian@gmail.com>
2013-09-28 21:40:12 +0800

    export an api

--- WeCampus/WeCampus/src/main/java/com/westudio/wecampus/ui/intro/IntroFragment.java
+++ WeCampus/WeCampus/src/main/java/com/westudio/wecampus/ui/intro/IntroFragment.java
@@ -1,17 +1,21 @@
 package com.westudio.wecampus.ui.intro;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.viewpagerindicator.UnderlinePageIndicator;
 import com.westudio.wecampus.R;
 import com.westudio.wecampus.ui.base.BaseFragment;
+import com.westudio.wecampus.ui.main.MainActivity;
+import com.westudio.wecampus.util.Utility;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,7 +23,7 @@ import java.util.List;
 /**
  * Created by nankonami on 13-9-18.
  */
-public class IntroFragment extends BaseFragment {
+public class IntroFragment extends BaseFragment implements View.OnClickListener{
 
     private static final int[] BG_IMAGE_IDS = {
             R.drawable.intro_pager_01, R.drawable.intro_pager_02, R.drawable.intro_pager_03
@@ -28,6 +32,8 @@ public class IntroFragment extends BaseFragment {
     private View view;
     private ViewPager viewPager;
     private UnderlinePageIndicator pageIndicator;
+    private Button btnNotNow;
+    private Button btnogin;
 
     private Activity activity;
 
@@ -76,6 +82,20 @@ public class IntroFragment extends BaseFragment {
         pageIndicator.setViewPager(viewPager, 0);
         pageIndicator.setFades(false);
 
+        btnNotNow = (Button) view.findViewById(R.id.intro_no_login);
+        btnNotNow.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.intro_no_login: {
+                startActivity(new Intent(getActivity(), MainActivity.class));
+                getActivity().finish();
+                break;
+            }
+        }
     }
 }
