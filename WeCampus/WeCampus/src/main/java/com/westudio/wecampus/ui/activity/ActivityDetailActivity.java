84842abdc90e4c90d27e63858204977efc2cc0a8
Tony <coolhzb@163.com>
2013-10-04 22:04:42 +0800

    start the activity detail part

---
+++ WeCampus/WeCampus/src/main/java/com/westudio/wecampus/ui/activity/ActivityDetailActivity.java
@@ -0,0 +1,29 @@
+package com.westudio.wecampus.ui.activity;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.westudio.wecampus.ui.base.BaseDetailActivity;
+
+/**
+ * Created by nankonami on 13-10-4.
+ * Activity that display the detail of activity
+ */
+public class ActivityDetailActivity extends BaseDetailActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
