bbb7ac8edd312362e97907c7b7243b0c3961065a
Tony <coolhzb@163.com>
2013-11-25 20:03:33 +0800

    活动列表部份基本上没什么问题了。。。

---
+++ WeCampus/WeCampus/src/main/java/com/westudio/wecampus/util/ListViewUtils.java
@@ -0,0 +1,47 @@
+package com.westudio.wecampus.util;
+
+import android.os.Build;
+import android.widget.ListView;
+
+/**
+ * Created by nankonami on 13-11-25.
+ */
+public class ListViewUtils {
+
+    private ListViewUtils() {
+
+    }
+
+    /**
+     * 滚动列表到顶端
+     *
+     * @param listView
+     */
+    public static void smoothScrollListViewToTop(final ListView listView) {
+        if (listView == null) {
+            return;
+        }
+        smoothScrollListView(listView, 0);
+        listView.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                listView.setSelection(0);
+            }
+        }, 200);
+    }
+
+    /**
+     * 滚动列表到position
+     *
+     * @param listView
+     * @param position
+     */
+    public static void smoothScrollListView(ListView listView, int position) {
+        if (Build.VERSION.SDK_INT > 7) {
+            listView.smoothScrollToPositionFromTop(0, 0);
+        } else {
+            listView.setSelection(position);
+        }
+    }
+}
