758a570d5c96cbc2aaf5ad2a63a8d6c15f4c9cd4
Tony <coolhzb@163.com>
2013-12-13 00:10:45 +0800

    继续更新leftmenu部份。。。

--- WeCampus/WeCampus/src/main/java/com/westudio/wecampus/ui/main/LeftMenuFragment.java
+++ WeCampus/WeCampus/src/main/java/com/westudio/wecampus/ui/main/LeftMenuFragment.java
@@ -140,6 +140,7 @@ public class LeftMenuFragment extends Fragment implements View.OnClickListener,
         mUserSection.setVisibility(View.VISIBLE);
         mBtnSignIn.setVisibility(View.GONE);
         mBtnSignOut.setVisibility(View.VISIBLE);
+        mBtnEdit.setVisibility(View.VISIBLE);
         if(Constants.IMAGE_NOT_FOUND.equals(mUser.avatar)) {
             if(Constants.MALE.equals(mUser.gender)) {
                 ivAvatar.setImageBitmap(ImageUtil.getRoundedCornerBitmap(BitmapFactory.decodeResource(
@@ -155,7 +156,9 @@ public class LeftMenuFragment extends Fragment implements View.OnClickListener,
                 @Override
                 public void onResponse(ImageLoader.ImageContainer imageContainer, boolean b) {
                     Bitmap data = imageContainer.getBitmap();
-                    ivAvatar.setImageBitmap(ImageUtil.getRoundedCornerBitmap(data));
+                    if(data != null) {
+                        ivAvatar.setImageBitmap(ImageUtil.getRoundedCornerBitmap(data));
+                    }
                 }
 
                 @Override
@@ -270,7 +273,7 @@ public class LeftMenuFragment extends Fragment implements View.OnClickListener,
             app.getAccountMgr().clearAccountInfo();
             WeCampusApi.logout(LeftMenuFragment.this, id, this, this);
 
-            if ( progressDialog == null) {
+            if (progressDialog == null) {
                 progressDialog = new ProgressDialog(getActivity());
             }
             progressDialog.setMessage(getString(R.string.please_wait));
