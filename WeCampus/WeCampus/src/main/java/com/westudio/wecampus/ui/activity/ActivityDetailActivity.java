2f0b1d50ad855360319cd1c7a6428735cc9170cd
zhujunguan <jgzhu@ctrip.com>
2014-01-03 17:50:50 +0800

    未登录时，详情界面屏蔽menu键 fix #56

--- WeCampus/WeCampus/src/main/java/com/westudio/wecampus/ui/activity/ActivityDetailActivity.java
+++ WeCampus/WeCampus/src/main/java/com/westudio/wecampus/ui/activity/ActivityDetailActivity.java
@@ -648,7 +648,7 @@ public class ActivityDetailActivity extends BaseGestureActivity implements OnRef
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_MENU) {
-            if (activity != null) {
+            if (activity != null && BaseApplication.getInstance().hasAccount) {
                 Intent i = new Intent(this, ShareMenuActivity.class);
                 i.putExtra(ShareMenuActivity.ACTIVITY_ID, activityId);
                 i.putExtra(ShareMenuActivity.CAN_JOIN, activity.can_join);
